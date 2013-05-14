1f55d7f9335c7e0eedd407791d8f9eeadd465955
theManda <jorge@bsdchile.cl>
2013-05-14 10:23:13 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -6,24 +6,25 @@ __author__ = "theManda"
 from ConfigParser import ConfigParser
 from base import Singleton
 
-cfg = dict()
-cfg['core'] = {'path': '/usr/local/deltafs',
-               'schedule_time': 'montly'}
 
-cfg['database'] = {'dbname': 'delta.db',
-                   'provider': 'sqlite'}
 
 
 class Config(Singleton):
 
-    global cfg
+    cfg = {}
+    cfg['core'] = {}
+    cfg['core']['path'] = '/usr/local/deltafs'
+    cfg['core']['schedule_time'] = 'montly'
+    cfg['database'] = {}
+    cfg['database']['dbname'] = 'delta.db'
+    cfg['database']['provider'] = 'sqlite'
 
     def __init__(self, cfg_file):
         config = ConfigParser()
         config.read(cfg_file)
-        for section in cfg.iteritems():
-            for k, v in section.iteritems():
-                cfg[section] = dict(k=v)
+        for section in cfg.iterkeys():
+            for key in cfg[section].iteritems():
+                cfg[section][key] = config.get(section, key)
 
     def get(self, item):
         return self.__getattribute__(item)
