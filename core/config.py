1bcd3d920c50533fe2181552a356afc10e2d727a
Jorge Medina <jorge@bsdchile.cl>
2013-05-25 21:51:21 +0200

    modified:   core/config.py

--- core/config.py
+++ core/config.py
@@ -3,7 +3,7 @@ __revision__ = "$"
 __version__ = "$"
 __author__ = "theManda"
 
-from ConfigParser import ConfigParser, RawConfigParser
+from ConfigParser import ConfigParser, RawConfigParser, NoSectionError, ParsingError, Error, NoOptionError,
 from base import Singleton
 
 
@@ -18,12 +18,7 @@ class Config(Singleton):
     cfg['database']['provider'] = 'sqlite'
 
     def __init__(self, cfg_file):
-        config = ConfigParser()
-        config.read(cfg_file)
-        for section in self.cfg.iterkeys():
-            for key, value in self.cfg[section].iteritems():
-                self.cfg[section][key] = config.get(section, key)
-                print self.cfg[section][key]
+        self._validate_cfg(cfg_file)
 
     def _create_cfg(self, cfg_file):
         config = RawConfigParser()
@@ -34,9 +29,27 @@ class Config(Singleton):
         with open(cfg_file, 'wb') as conf:
             config.write(conf)
 
-
     def _validate_cfg(self, cfg_file):
-        pass
+        if not self._exist_cfg(cfg_file):
+            self._create_cfg(cfg_file)
+
+        config = ConfigParser()
+        config.read(cfg_file)
+        try:
+            for section in self.cfg.iterkeys():
+                for key, value in self.cfg[section].iteritems():
+                    self.cfg[section][key] = config.get(section, key)
+                    print self.cfg[section][key]
+        except NoSectionError:
+            print("Section Error")
+
+    def _exist_cfg(self, cfg_file):
+        try:
+            with open(cfg_file):
+                return True
+        except IOError:
+            print('config file not exist')
+            return False
 
     def get(self, section, item):
         pass
