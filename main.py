cbbee898b58d4bbcf04b9ad4ad26dfd578929a24
Jorge Medina <jorge@bsdchile.cl>
2013-05-26 22:37:57 +0200

    deleted:    core/__init__.py 	deleted:    core/base.py 	deleted:    core/collector.py 	deleted:    core/config.py 	deleted:    main.py 	deleted:    persistence/__init__.py 	deleted:    persistence/delta.db 	deleted:    persistence/orm.py 	deleted:    persistence/store.py 	deleted:    tests/__init__.py 	setup.py 	src/

--- main.py
+++
@@ -1,9 +0,0 @@
-__copyright__ = "Copyright (C) 2013 Jorge A. Medina"
-__revision__ = "$"
-__version__ = "$"
-__author__ = "theManda"
-
-from core.collector import Collector
-
-if __name__ == '__main__':
-    agent = Collector().run()
